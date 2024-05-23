46d33a043e7df708f478f2708088597d1afa5953
AyoubMounim <ayoub.mounim@gmail.com>
2024-05-23 16:04:37 +0200

    mu_colors

---
+++ lua/mu_colors.lua
@@ -0,0 +1,33 @@
+
+local colors = {
+  default_bg = '#212121',
+  default_fg = '#E0E0E0',
+  cursor_line = '#313131',
+  brown = '#BCAAA4',
+  grey = '#616161',
+  light_grey = '#BDBDBD',
+  blue = '#64B5F6',
+  light_blue = '#B3E5FC',
+  dark_blue = '#2196F3',
+  deep_purple = '#9575CD',
+  cyan = '#4DD0E1',
+  dark_cyan = '#00BCD4',
+  teal = '#4DB6AC',
+  indigo = '#7986CB',
+  green = '#81C784',
+  light_green = '#AED581',
+  dark_green = '#4CAF50',
+  lime = '#CDDC39',
+  light_yellow = '#FFF59D',
+  yellow = '#FFF176',
+  dark_yellow = '#FFEB3B',
+  amber = '#FFC107',
+  orange = '#FFB74D',
+  dark_orange = '#FF8A65',
+  red = '#E57373',
+  pink = '#F06292',
+  purple = '#BA68C8',
+}
+
+return colors
+
